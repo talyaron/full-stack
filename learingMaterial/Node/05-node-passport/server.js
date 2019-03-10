1b4de65d056bb2f0d96de708cf9fbf8e21cb12db
Tal Yaron <tal.yaron@gmail.com>
2019-03-10 13:50:54 +0200

    passport 2

--- learingMaterial/Node/05-node-passport/server.js
+++ learingMaterial/Node/05-node-passport/server.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const session = require("express-session");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://nodechef-mongo-6060:Nos8DzRE5xYRUPCERykiNf9a20THxV@db-nodechef-mongo-6060.nodechef.com:5384/nodechef-mongo');
-// const MongoClient = require('mongodb').MongoClient;
-// const dbUrl = 'mongodb://localhost:27017/mydb';
+mongoose
+    .connect('mongodb://nodechef-mongo-6060:Nos8DzRE5xYRUPCERykiNf9a20THxV@db-nodechef-mongo-6060.nodechef.com:5384/nodechef-mongo',
+        { useNewUrlParser: true });
+
 
 
 var db = mongoose.connection;
@@ -27,13 +28,15 @@ UserSchema.methods.validPassword = function (pwd) {
 
 var User = mongoose.model('User', UserSchema);
 
-
+// create new entry
 // const talyaron = new User({ username: 'talyaron', password: '123' });
 // talyaron.save(err => {
 //     if (err) return console.error(err);
 //     console.log('user wirten succesfuly to db')
 // })
 
+
+//configure local startegy (username and password)
 
 passport.use(new localStrategy(
     function (username, password, done) {
@@ -56,43 +59,62 @@ passport.use(new localStrategy(
 
 const app = express();
 
-
-
-
 app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
+app.use(session({ resave: true, saveUninitialized: true, secret: 'keyboard cat' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 passport.serializeUser(function (user, done) {
-    done(null, user);
+    console.log('serializeUser:', user.username)
+    done(null, user.username);
 });
 
 passport.deserializeUser(function (user, done) {
+    console.log('deserializeUser:', user)
     done(null, user);
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
-})
 
-app.get('/users/:id', (req, res) => {
-    res.send(`<h1>This is a page for user with id: ${req.params.id}</h1>`)
-})
+app.get('/users/:id',
 
+    (req, res) => {
+        console.log(req.user)
+
+        if (req.user == req.params.id) {
+            res.send(`<h1>This is a page for user with id: ${req.params.id}</h1>`)
+        } else {
+            res.send(`<h1 style='color:red'>You dont have a permission to see info for user: ${req.params.id}</h1>`)
+        }
+
+
+    }
+)
+
+//without failure option
 app.post('/login',
     passport.authenticate('local'),
     (req, res) => {
         res.redirect(`/users/${req.user.username}`)
         console.log('log in')
-        // res.send('logged')
+
     }
 )
 
+//with failure option
+app.get('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.redirect('/'); }
+        req.login(user, function (err) {
+            if (err) { return next(err); }
+            return res.redirect(`/users/${req.user.username}`);
+        });
+    })(req, res, next);
+});
 
-// app.use(express.static('public'));
+
 
 let port = process.env.PORT || 3000;
 
