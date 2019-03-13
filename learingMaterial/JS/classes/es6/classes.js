1aac4522f3aedd22f43b6d8642bc90fae97baf3c
Tal Yaron <tal.yaron@gmail.com>
2019-03-13 19:46:03 +0200

    class

--- learingMaterial/JS/classes/es6/classes.js
+++ learingMaterial/JS/classes/es6/classes.js
@@ -22,7 +22,27 @@ class Pets {
 }
 
 let cat = new Pets('cat', 'small', 15, 200);
-let dog = new Pets ('dog','medium', 12, 300);
+let dog = new Pets('dog', 'medium', 12, 300);
 cat.callNameNSize();
 console.log(cat.costOfFood);
 console.log(cat.calculteCostOfFood())
+
+//getters
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    // Getter
+    get area() {
+        return this.calcArea();
+    }
+    // Method
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10, 10);
+
+console.log(square.area); // 100
