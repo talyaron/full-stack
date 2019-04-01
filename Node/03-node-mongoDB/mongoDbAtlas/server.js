3cbc826689694975c22b84574343aeaef523b393
Tal Yaron <tal.yaron@gmail.com>
2019-04-01 17:24:30 +0300

    monodb new

--- Node/03-node-mongoDB/mongoDbAtlas/server.js
+++ Node/03-node-mongoDB/mongoDbAtlas/server.js
@@ -1,19 +1,49 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://talyaron:Yuva1)))@tal-test1-m39if.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-const assert = require('assert');
+var url = "mongodb+srv://talyaron:Yuva1))))@tal-test1-m39if.mongodb.net/test?retryWrites=true";
 
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+
+
+
+// MongoClient.connect(url, function (err, db) {
+//     if (err) throw err;
+//     console.log("Database created!");
+//     db.close();
+// });
+
+
+
+const client = new MongoClient(url, { useNewUrlParser: true });
 client.connect(err => {
-
-    const collection = client.db("tal-db-test1").collection("dogs");
+    const collection = client.db("test").collection("devices");
     // perform actions on the collection object
-    collection.insertOne({ name: 'Browny' }, (err, res) => {
-        assert.equal(null, err);
-        console.log('Item inserted')
-    })
-
     client.close();
-
-
 });
+
+// MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+
+//     var mysort = { name: -1 };
+//     dbo.collection("customers").find().sort(mysort).toArray(function (err, result) {
+//         if (err) throw err;
+//         res.send(result)
+//         db.close();
+//     });
+// });
+
+
+app.listen(port, () => console.log(`server listening on port ${port}!`))
