73c659cd6422b801a5853ce22494b6b9c5fd11c8
Tal Yaron <tal.yaron@gmail.com>
2019-10-28 21:29:28 +0200

    express-handelbars-CRUD

---
+++ Node/03-node-mongoDB/mongoDB-CRUD-handlebars/routes/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
+
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/sampleDB";
+
+router.get("/", (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+
+    const students = db.db("sampleDB").collection("students");
+
+    students
+      .find()
+      .sort({insertTime:-1})
+      .toArray()
+      .then(result => {
+        if (err) throw err;
+
+        res.render("students", { students: result });
+        db.close();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+});
+
+router.post("/addStudent", (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    let newStudent = req.body;
+    const students = db.db("sampleDB").collection("students");
+    newStudent.insertTime = new Date().getTime();
+    newStudent.average = "not set";
+
+    students
+      .insertOne(newStudent)
+      .then(result => {
+        if (err) throw err;
+        res.redirect("/");
+      
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      db.close();
+  });
+});
+
+router.delete('/deleteStudent',(req, res)=>{
+  console.log('delete:')
+  
+  const {studentId} = req.body;
+  
+ 
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    
+    const students = db.db("sampleDB").collection("students");
+    
+
+    students
+      .deleteOne( {_id: ObjectId(studentId)})
+      .then(result => {
+        if (err) throw err;
+        console.log(`students deleted: ${result.deletedCount}`);
+        res.redirect(303, "/");
+      
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      db.close();
+  });
+})
+
+router.put('/updateAverage', (req,res)=>{
+  const {grade, studentId} = req.body;
+  
+  
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    
+    const students = db.db("sampleDB").collection("students");    
+
+    students
+      .updateOne( {_id: ObjectId(studentId)},{$set: {"average": grade}})
+      .then(result => {
+        if (err) throw err;
+        if(result.modifiedCount == 1){
+          res.send({ok:"OK"});
+        } else {
+          res.send({error:'update error'})
+        }
+        
+      
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      db.close();
+  });
+})
+
+module.exports = router;
