20ea29e2528bdc5e420b43bbac1ac46ea3b86268
Tal Yaron <tal.yaron@gmail.com>
2020-03-13 09:48:10 +0200

    update

--- SPA/React/react-redux/redux2/src/index.js
+++ SPA/React/react-redux/redux2/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'; //asynchronic update of app-state
+import { createStore, applyMiddleware} from "redux";
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
