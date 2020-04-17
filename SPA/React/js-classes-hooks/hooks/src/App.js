4c0296c1c3ec2a03c2384b3f3a18963162d90c6f
Tal Yaron <tal.yaron@gmail.com>
2020-04-17 11:39:59 +0300

    hooks login ready

--- SPA/React/js-classes-hooks/hooks/src/App.js
+++ SPA/React/js-classes-hooks/hooks/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { login, logout, listenToLogin } from './functions/firebase';
+
+function App () {
+
+  const [isShow, setIsShow] = useState(false);
+  const [img, setImg] = useState('')
+ 
+  useEffect(()=>{
+    listenToLogin(setIsShow, setImg)
+  },[])
+  
+
+    return (
+      <div className="page">
+        <h1>Pinstegram</h1>
+        <h2>Login</h2>
+        <form onSubmit={login}>
+
+          <button type='submit'>Login to Google</button>
+        </form>
+        <hr />
+        <button onClick={logout}>Logout</button>
+        {isShow ?
+          <img id='userImg' src={img} />
+          : null
+        }
+      </div>
+    )
+  
 }
 
 export default App;
---
+++ SPA/React/js-classes-hooks/hooks/src/functions/firebase.js
@@ -0,0 +1,82 @@
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0cCt2TyZOdo4MaxBxN2LAqwoWKPIMRYo",
+    authDomain: "fs3-pintegram.firebaseapp.com",
+    databaseURL: "https://fs3-pintegram.firebaseio.com",
+    projectId: "fs3-pintegram",
+    storageBucket: "fs3-pintegram.appspot.com",
+    messagingSenderId: "611402642005",
+    appId: "1:611402642005:web:9d2285edf62cd01d135adc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const DB = firebase.firestore();
+// export const storage = firebase.storage().ref();
+
+export function login(e) {
+    e.preventDefault();
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        console.error(error)
+        // ...
+    });
+
+}
+
+export function listenToLogin(setIsShow, setImg) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(user)
+            const { photoURL } = user;
+            console.log(typeof photoURL)
+
+            if (photoURL) {
+
+                setIsShow(true);
+                setImg(photoURL)
+
+            }
+            console.log('User is signed in.');
+        } else {
+            console.log(' No user is signed in.');
+            setIsShow(false);
+            setImg('')
+        }
+    });
+}
+
+
+export function logout() {
+
+    firebase.auth().signOut().then(function () {
+        console.info('Logout succesfull')
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+}
