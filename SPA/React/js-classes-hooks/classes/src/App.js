a6752514ef9772c2b6f36456502d1ceb92ea499a
Tal Yaron <tal.yaron@gmail.com>
2020-04-17 11:05:15 +0300

    react login ready

--- SPA/React/js-classes-hooks/classes/src/App.js
+++ SPA/React/js-classes-hooks/classes/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {login, logout, listenToLogin} from './functions/firebase';
+
+class App extends Component  {
+  constructor(props){
+    super(props)
+    this.state = {isShow:false, img:''};
+    this.handleShowPicture = this.handleShowPicture.bind(this);
+  }
+
+  handleShowPicture(isShow, img){
+    this.setState({isShow,img})
+  }
+
+  componentDidMount(){
+   listenToLogin({picHandler:this.handleShowPicture});
+  }
+
+  render(){
+    return(
+      <div className="page">
+      <h1>Pinstegram</h1>
+      <h2>Login</h2>
+      <form onSubmit={login}>
+         
+          <button type='submit'>Login to Google</button>
+      </form>
+      <hr />
+      <button onClick={logout}>Logout</button>
+      {this.state.isShow?
+      <img id='userImg' src={this.state.img} />
+        :null
+    }
+      </div>
+    )
+  }
 }
 
 export default App;
