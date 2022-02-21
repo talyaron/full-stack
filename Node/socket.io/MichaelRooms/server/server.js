6bc70420403325450d7b639dc95eb995347842ba
Tal Yaron <tal.yaron@gmail.com>
2022-02-21 17:11:08 +0200

    cont

--- Node/socket.io/MichaelRooms/server/server.js
+++ Node/socket.io/MichaelRooms/server/server.js
@@ -154,6 +154,7 @@ app.use(cors());
 let roomsNamesObj = {};
 
 io.sockets.on('connection', function (socket) {
+    
     socket.on('subscribe', function (room) {
         console.log('joining room', room);
         socket.join(room);
